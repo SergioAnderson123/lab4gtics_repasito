b4174b72e80c935372fb5beb5d6c787f08fe6bf1
SergioAnderson123 <a20223291@pucp.edu.pe>
2026-05-06 23:38:30 -0500

    Initial commit

---
+++ src/test/java/com/pucp/gtics/lab4gtics/Lab4gticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucp.gtics.lab4gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4gticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
